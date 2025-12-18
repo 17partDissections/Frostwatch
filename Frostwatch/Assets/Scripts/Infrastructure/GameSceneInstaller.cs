6884fd7334b79a1041f8bb67f60bee6f1d9a553e
17partDissections <swalekjr@gmail.com>
2025-12-18 15:35:07 +0300

    Shaders and Zenject
    Added project and scenes installers for Zenject.
    Added Resources for Zenject and Outline shader

---
+++ Frostwatch/Assets/Scripts/Infrastructure/GameSceneInstaller.cs
@@ -0,0 +1,8 @@
+using Zenject;
+
+public class GameSceneInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+    }
+}
---
+++ Frostwatch/Assets/Scripts/Infrastructure/MainProjectInstaller.cs
@@ -0,0 +1,8 @@
+using Zenject;
+
+public class MainProjectInstaller : MonoInstaller
+{
+public override void InstallBindings()
+    {
+    }
+}
