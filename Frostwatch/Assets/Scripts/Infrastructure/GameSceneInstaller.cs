84746fc6a79d8565148cc530f3c50068e8df4d35
17partDissections <swalekjr@gmail.com>
2025-12-20 21:44:18 +0300

    Editing Game scene
    Added new SFX sounds
    Edited models import settings
    Edited item system

--- Frostwatch/Assets/Scripts/Infrastructure/GameSceneInstaller.cs
+++ Frostwatch/Assets/Scripts/Infrastructure/GameSceneInstaller.cs
@@ -1,8 +1,32 @@
 using Zenject;
+using UnityEngine;
+using Q17pD.Frostwatch.Player;
+using Q17pD;
 
 public class GameSceneInstaller : MonoInstaller
 {
+    [SerializeField] private AudioHandler _audioHandler;
+    [SerializeField] private Player _player;
     public override void InstallBindings()
     {
+        BindAudioHandler();
+        BindPlayer();
+    }
+
+    private void BindAudioHandler()
+    {
+        Container
+            .Bind<AudioHandler>()
+            .FromInstance(_audioHandler)
+            .AsSingle()
+            .NonLazy();
+    }
+        private void BindPlayer()
+    {
+        Container
+            .Bind<Player>()
+            .FromInstance(_player)
+            .AsSingle()
+            .NonLazy();
     }
 }
--- Frostwatch/Assets/Scripts/Item.cs
+++ Frostwatch/Assets/Scripts/Item.cs
@@ -1,30 +1,53 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
+using Zenject;
 
 namespace Q17pD.Frostwatch
 {
     public class Item : MonoBehaviour
     {
-        [SerializeField] private string _itemName;
-        private Outline _outline;
         [SerializeField] private int _itemIndex;
+        [SerializeField] private string _itemName;
+        [SerializeField] private string _itemDescription;
+        [SerializeField] private bool _collectable;
         [Range(0, 5)][SerializeField] private int _amount = 1;
-         [SerializeField] private bool _collectable;
-        private void Start() 
+        [SerializeField] private Transform  _moveFinalTransform;
+        [SerializeField] private AudioClip _itemEnterSound, _itemPickUpSound, _itemDropSound;
+        private Outline _outline;
+        private Vector3 _originalPos;
+        private Player.Player _player;
+        private AudioHandler _audioHandler;
+
+       [Inject] private void Construct(Player.Player player, AudioHandler audioHandler)
         {
             _outline = GetComponent<Outline>();
             _outline.enabled = false;
+            if(_collectable) 
+            {
+                _originalPos = transform.position;
+                _player = player;
+            }
+            _audioHandler = audioHandler;
         }
+
         private void OnMouseEnter()
         {
             _outline.enabled = true;
+            _audioHandler.PlaySFX(_itemEnterSound, 0);
+            if(_collectable) transform.DOMove(_moveFinalTransform.position, 0.1f);
         }
         private void OnMouseExit()
         {
             _outline.enabled = false;
+            if(_collectable) transform.DOMove(_originalPos, 0.1f);
         }
         private void OnMouseDown()
         {
+            if(_collectable)
+            {
+                _audioHandler.PlaySFX(_itemPickUpSound, 0);
+            }
         }
     }
 }
