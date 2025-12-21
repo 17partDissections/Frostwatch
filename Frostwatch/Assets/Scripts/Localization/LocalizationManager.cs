29d58f5627a16af4f194eccfc60f433309a7cbe0
17partDissections <swalekjr@gmail.com>
2025-12-21 12:01:23 +0300

    Localization
    Added localization scripts

---
+++ Frostwatch/Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using DFTGames.Localization;
+
+namespace MBW.WaST
+{
+    public class LocalizationManager : MonoBehaviour
+    {
+        private List<SystemLanguage> _languages = new List<SystemLanguage> { SystemLanguage.English, SystemLanguage.Russian, SystemLanguage.French, SystemLanguage.German, SystemLanguage.Unknown };
+        private int _languageIndex;
+
+        public void ChangeIndex(int newIndex) { _languageIndex = newIndex; }
+        public void ChangeLanguage()
+        {
+            Localize.SetCurrentLanguage(_languages[_languageIndex]);
+            //LocalizeImage.SetCurrentLanguage(_languages[_languageIndex]);
+        }
+    }
+}
